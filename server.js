a596aa6251f1507a5a833cc7b803ce50270219ad
Eric J <34559118+jiapurdue@users.noreply.github.com>
2018-05-05 11:04:02 -0400

    2

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
 
-// Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+class Reservation {
+    constructor(name,phoneNum,email,id) {
+        this.name = name;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.id = id;
+    }
+};
+
+const people = [];
+
+const p1 = new Reservation('Tom',7323550000,'tom@gmail.com',1)
+
+people.push(p1);
